42043043f93eccad3a9a7f823a1c2e428f437bd3
tanikawa-tomoya <tanikawa.tomoya@outlook.jp>
2025-11-28 21:11:14 +0900

    Add ambient floating symbols to agon index page

--- root/js/page/agon-index/main.js
+++ root/js/page/agon-index/main.js
@@ -17,7 +17,8 @@
                                                       { src: '/js/service-app/loading/main.js' },
                                                       { src: '/js/service-app/button/main.js' },
                                           { src: '/js/page/agon-index/job-view.js' },
-                                          { src: '/js/page/agon-index/job-background.js' }]);
+                                          { src: '/js/page/agon-index/job-background.js' },
+                                          { src: '/js/page/agon-index/job-ambient-symbols.js' }]);
 
       this.initConfig();
       
@@ -35,6 +36,7 @@
 
       new window.AgonIndex.JobView (this).loadPage();
       new window.AgonIndex.JobBackground(this).setup();
+      new window.AgonIndex.JobAmbientSymbols(this).setup();
     }
 
     updateAgonIndex()
